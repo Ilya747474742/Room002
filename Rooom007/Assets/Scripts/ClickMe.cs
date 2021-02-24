c56a5c7c1c7735c4958d6be3bbdd9a613efe935a
Ilya747474742 <68121715+Ilya747474742@users.noreply.github.com>
2021-02-24 12:56:49 +0700

    123

---
+++ Rooom007/Assets/Scripts/ClickMe.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ClickMe : MonoBehaviour
+{
+    public Click gm;
+    public void Clickk()
+    {
+        gm.Gold += gm.goldPerClick;
+    }
+
+}
