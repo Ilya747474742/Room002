38af442f752c38f268f33faf8b93669e06b2c9ec
Ilya747474742 <68121715+Ilya747474742@users.noreply.github.com>
2021-02-25 00:15:10 +0700

    123

---
+++ Rooom007/Assets/Scripts/AvtoMoney.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvtoMoney : MonoBehaviour
+{
+    public Click gm;
+    public void Avto() 
+    {
+        
+        if (gm.Gold >= 5)
+        {
+           
+            gm.Gold -= 5;
+            StartCoroutine(IdleFarm0());
+        }
+
+
+    }
+    IEnumerator IdleFarm0()
+    {
+        while (true)
+        {
+            gm.Gold += 1;
+            yield return new WaitForSeconds(1);
+           
+        }
+
+
+    }
+
+}
